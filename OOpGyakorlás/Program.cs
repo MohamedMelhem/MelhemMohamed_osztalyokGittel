0a76986ee7fa128e4f17f4a7526ed5d38941d0c9
Mohamed Melhem <melhemmohamed12@gmail.com>
2025-02-10 19:36:25 +0100

    Kémek Feladat Kész
    Minden Kész:D
    
    (az esej ly vel van csak a feladatba el lett irva)
    
    :)

--- OOpGyakorlás/Program.cs
+++ OOpGyakorlás/Program.cs
@@ -9,54 +9,62 @@ namespace OOpGyakorlás
 			//Konyv konyv1 = new Konyv("A kis herceg", "Antonia", 1943 ,2500 , 96);
 			//Konyv konyv2 = new("Ez meg keszul", "Én");
 
-   //         Console.WriteLine(konyv1.ToString() + "\n" + konyv2.ToString());
+			//         Console.WriteLine(konyv1.ToString() + "\n" + konyv2.ToString());
 
 
 			//konyv1.Kedvezmeny(20);
 			//konyv1.Arnoveles(1000);
 
-   //         Console.WriteLine();
-   //         Console.WriteLine(konyv1.ToString() + "\n" + konyv2.HanyOldalMaradt(20));
-			
+			//         Console.WriteLine();
+			//         Console.WriteLine(konyv1.ToString() + "\n" + konyv2.HanyOldalMaradt(20));
+
 
 			//Film film1 = new Film("A kis herceg", "Antonia", 1943, "Fantasy", true);
-   //         Film film2 = new("Ez meg keszul", "Én");
-   //         Console.WriteLine(film1.ToString() + "\n" + film2.ToString());
+			//         Film film2 = new("Ez meg keszul", "Én");
+			//         Console.WriteLine(film1.ToString() + "\n" + film2.ToString());
 			//Console.WriteLine();
 
 			//Videojatek jatekos = new Videojatek("Ferencz", 100);
-   //         Videojatek jatekos2 = new("Béla", 200);
-   //         Console.WriteLine(jatekos.ToString() + "\n" + jatekos2.ToString());
+			//         Videojatek jatekos2 = new("Béla", 200);
+			//         Console.WriteLine(jatekos.ToString() + "\n" + jatekos2.ToString());
 			//Videojatek jatekos3 = new Videojatek("lovag", 100, 100, 100);
-   //         Videojatek jatekos4 = new("kardos", 200, 200, 200);
+			//         Videojatek jatekos4 = new("kardos", 200, 200, 200);
 			//jatekos.Gyogyulas(20);
 			//jatekos2.SzintLepes();
 			//jatekos3.Tamadas();
-   //         Console.WriteLine(jatekos3.ToString() + "\n" + jatekos4.ToString());
-   //         Console.WriteLine(jatekos);
-   //         Console.WriteLine(jatekos2);
+			//         Console.WriteLine(jatekos3.ToString() + "\n" + jatekos4.ToString());
+			//         Console.WriteLine(jatekos);
+			//         Console.WriteLine(jatekos2);
 
 
 			//Urhajo spaceX = new Urhajo("Tesla", 100, 20, 100);
 
-   //         Console.WriteLine(spaceX);
-   //         Console.WriteLine("A Tesla megálllt tankolni");
+			//         Console.WriteLine(spaceX);
+			//         Console.WriteLine("A Tesla megálllt tankolni");
 			//spaceX.Tankolas(20); //Megtankoltam
-   //         Console.WriteLine(spaceX);
+			//         Console.WriteLine(spaceX);
 
 			//Urhajo Testhajo = new Urhajo("RobotImre", 1000);
-   //         Console.WriteLine(Testhajo);
-   //         Console.WriteLine("A TestHajo Elindul");
+			//         Console.WriteLine(Testhajo);
+			//         Console.WriteLine("A TestHajo Elindul");
 			//Testhajo.Indulas();
-   //         Console.WriteLine(Testhajo);
+			//         Console.WriteLine(Testhajo);
 			//Testhajo.Landolas();
-   //         Console.WriteLine(Testhajo);
-   //         Console.WriteLine("Landolt");
+			//         Console.WriteLine(Testhajo);
+			//         Console.WriteLine("Landolt");
 
 
-
-
-
+			Kemek kem = new Kemek("Oroszlan", "magyarorszag",6,80);
+            Console.WriteLine(kem);
+			kem.SikerEsejNovelese(10); // itt növeltem az esélyét
+            Console.WriteLine(" A kém Talált valami hasznosat így nőtt az esélye");
+			Console.WriteLine(kem);
+            Console.WriteLine("--------------------------------------------------------------");
+			Kemek orosz = new Kemek("VorosHollo", "Oroszorszag");
+            Console.WriteLine(orosz);
+			orosz.KuldetesInditasa();
+			orosz.VeszelySzintNovelese(10);
+			Console.WriteLine(orosz);
         }
 	}
 }
